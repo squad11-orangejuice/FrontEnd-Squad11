45773b60f8ce48126c134cc39ed086e339ddcc15
Douglas dos Santos Oliveira <doug1306@gmail.com>
2024-01-23 14:05:37 -0300

    adicionando rotas  e theme Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
 export function App() {
   return (
-    <>
-      <h1> Hello world</h1>
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
