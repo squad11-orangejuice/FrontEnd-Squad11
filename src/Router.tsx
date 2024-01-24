9d2d70622fe6cb3a1ea661bc450731858ff5fc80
Douglas dos Santos Oliveira <doug1306@gmail.com>
2024-01-24 13:59:54 -0300

    Criação da rota de registro

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,11 +3,14 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import { SignIn } from './pages/SignIn'
 import { MyProjects } from './pages/MyProjects'
 import { Discover } from './pages/Discover'
+import { SignUp } from './pages/SignUp'
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<SignIn />} />
+      <Route path="/registro" element={<SignUp />} />
+
       <Route path="/projetos" element={<MyProjects />} />
       <Route path="/descobrir" element={<Discover />} />
       <Route path="*" element={<Navigate replace to="/" />} />
