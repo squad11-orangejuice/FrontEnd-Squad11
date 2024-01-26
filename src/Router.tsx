caace216f2d850de2c597e48a7a48274f9cdb1b1
Alanna Silva <alannacostas55@gmail.com>
2024-01-26 12:19:25 -0300

    Removendo Rota desnecessária e adicionando useIsMobile

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,7 +4,6 @@ import { SignIn } from './pages/SignIn'
 import { MyProjects } from './pages/MyProjects'
 import { Discover } from './pages/Discover'
 import { SignUp } from './pages/SignUp'
-import { ModalView } from './components/ModalView'
 
 export function Router() {
   return (
@@ -12,7 +11,6 @@ export function Router() {
       <Route path="/" element={<SignIn />} />
       <Route path="/registro" element={<SignUp />} />
       <Route path="/projetos" element={<MyProjects />} />
-      <Route path='/visualizar' element={<ModalView />} />
       <Route path="/descobrir" element={<Discover />} />
       <Route path="*" element={<Navigate replace to="/" />} />
     </Routes>
