b8a946ffa412d18c7a70317f3b6ab22061cfba58
Douglas dos Santos Oliveira <doug1306@gmail.com>
2024-01-26 13:36:59 -0300

    alteração do tipo de arquivo tsx para ts

---
+++ src/components/EditProjectButton/styles.ts
@@ -0,0 +1,37 @@
+import styled, { keyframes } from 'styled-components'
+import MenuItem from '@mui/material/MenuItem'
+import FileUploadIcon from '@mui/icons-material/FileUpload'
+
+const fadeIn = keyframes`
+  from {
+    transform: scale(0.9);
+  }
+  to {
+    transform: scale(1);
+  }
+`
+export const ButtonIconEdit = styled.div`
+  position: absolute;
+  top: 16px;
+  right: 16px;
+  z-index: 1;
+  &:hover {
+    animation: ${fadeIn} 0.3s ease;
+  }
+`
+export const ArrowIconEdit = styled(FileUploadIcon)`
+  position: absolute;
+  top: 38px;
+  right: 30px;
+  z-index: 99;
+  color: #fcfdff;
+`
+
+export const StyledMenuItem = styled(MenuItem)`
+  position: absolute;
+  width: 208px;
+  padding: 4px;
+  &:hover {
+    background: #ffcc99;
+  }
+`
