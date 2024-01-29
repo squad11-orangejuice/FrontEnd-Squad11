d68d5e5e7ed786a9d8172c8b15e898d13b231df7
Douglas dos Santos Oliveira <doug1306@gmail.com>
2024-01-29 20:25:07 -0300

    Criar componente de botão reutilizável

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,4 +1,5 @@
 import LoadingButton, { LoadingButtonProps } from '@mui/lab/LoadingButton'
+import { useTheme } from 'styled-components'
 
 interface IButton extends LoadingButtonProps {
   loading?: boolean
@@ -6,8 +7,25 @@ interface IButton extends LoadingButtonProps {
 }
 
 export function Button({ loading = false, title, ...rest }: IButton) {
+  const theme = useTheme()
+
   return (
-    <LoadingButton loading={loading} {...rest}>
+    <LoadingButton
+      loading={loading}
+      {...rest}
+      sx={{
+        backgroundColor:
+          title === 'CANCELAR'
+            ? '#D3D3D3'
+            : theme.colors['$color-secondary-100'],
+        '&:hover ': {
+          backgroundColor:
+            title === 'CANCELAR'
+              ? '#B0B0B0'
+              : theme.colors['$color-secondary-110'],
+        },
+      }}
+    >
       {title}
     </LoadingButton>
   )
