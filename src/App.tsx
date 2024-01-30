76ac18c57f8f34477f9f5c7e55e4e66e2b6896ee
Douglas dos Santos Oliveira <doug1306@gmail.com>
2024-01-30 17:22:44 -0300

    adicionando provider do react query

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,24 @@ import { Router } from './Router'
 import { BrowserRouter } from 'react-router-dom'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import { ModalContextProvider } from './context/ModalContext'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 export function App() {
   const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID
+  const queryClient = new QueryClient()
 
   return (
     <GoogleOAuthProvider clientId={googleClientId}>
-      <ThemeProvider theme={defaultTheme}>
-        <GlobalStyle />
-        <BrowserRouter>
-          <ModalContextProvider>
-            <Router />
-          </ModalContextProvider>
-        </BrowserRouter>
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={defaultTheme}>
+          <GlobalStyle />
+          <BrowserRouter>
+            <ModalContextProvider>
+              <Router />
+            </ModalContextProvider>
+          </BrowserRouter>
+        </ThemeProvider>
+      </QueryClientProvider>
     </GoogleOAuthProvider>
   )
 }
