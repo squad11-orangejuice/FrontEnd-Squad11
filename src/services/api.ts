ae1920dfe0b18c10cfce0cbee372349a46189774
Douglas dos Santos Oliveira <doug1306@gmail.com>
2024-01-31 15:25:58 -0300

    Criando requisição post para adicionar projetos

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,3 +1,4 @@
+import { convertBase64ToBlob } from '@/functions/covertStringInBlob'
 import { IProject, IRequestData } from '@/utils/types'
 import axios from 'axios'
 
@@ -24,21 +25,25 @@ export async function addProjects({
   title,
   description,
 }: IRequestData) {
+  const formData = new FormData()
+
+  const a = convertBase64ToBlob(imagem)
+  formData.append('titulo', title)
+  formData.append('link', link)
+  formData.append('descricao', description)
+
+  formData.append('imagem', a)
+
+  tags.forEach((tag, index) => {
+    formData.append(`tags[${index}]`, tag)
+  })
+
   return (
-    await axiosInstance.post(
-      '/projeto/novo',
-      {
-        titulo: title,
-        link,
-        descricao: description,
-        imagem,
-        tags,
+    await axiosInstance.post('/projeto/novo', formData, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/x-www-form-urlencoded',
       },
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      },
-    )
+    })
   ).data
 }
