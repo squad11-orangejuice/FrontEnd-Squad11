6e6db68eff91727438d2224a4358ee93b7e17bc9
Alanna Silva <alannacostas55@gmail.com>
2024-02-01 18:22:42 -0300

    Adicionando funcionalidade de Private e Public Route

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Navigate, Route, Routes } from 'react-router-dom'
 
 import { SignIn } from './pages/SignIn'
@@ -5,14 +6,41 @@ import { MyProjects } from './pages/MyProjects'
 import { Discover } from './pages/Discover'
 import { SignUp } from './pages/SignUp'
 
+const PrivateRoute = ({ children }) => {
+  const token = localStorage.getItem('bearerToken');
+  return token ? children : <Navigate to="/" />;
+};
+
+
+const PublicRoute = ({ children }) => {
+  const token = localStorage.getItem('bearerToken');
+  return !token ? children : <Navigate to="/projetos" />;
+};
+
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<SignIn />} />
-      <Route path="/registro" element={<SignUp />} />
-      <Route path="/projetos" element={<MyProjects />} />
-      <Route path="/descobrir" element={<Discover />} />
+      <Route path="/" element={
+        <PublicRoute>
+          <SignIn />
+        </PublicRoute>
+      } />
+      <Route path="/registro" element={
+        <PublicRoute>
+          <SignUp />
+        </PublicRoute>
+      } />
+      <Route path="/projetos" element={
+        <PrivateRoute>
+          <MyProjects />
+        </PrivateRoute>
+      } />
+      <Route path="/descobrir" element={
+        <PrivateRoute>
+          <Discover />
+        </PrivateRoute>
+      } />
       <Route path="*" element={<Navigate replace to="/" />} />
     </Routes>
-  )
+  );
 }
