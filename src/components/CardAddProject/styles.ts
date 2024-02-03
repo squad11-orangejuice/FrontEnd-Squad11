d350263de30caeadfdae102b24879bd5edd072bd
Douglas dos Santos Oliveira <doug1306@gmail.com>
2024-02-02 23:05:37 -0300

    colocar validação para quando aparecer o projeto se a imagem for menor na largura ele nao parecer a cor neutral 60

--- src/components/CardAddProject/styles.ts
+++ src/components/CardAddProject/styles.ts
@@ -3,7 +3,7 @@ import { styled } from 'styled-components'
 export const InputImage = styled.div`
   align-items: center;
   border-radius: 4px;
-  background-color: ${({ theme }) => theme.colors['$color-neutral-70']};
+  /* background-color: ${({ theme }) => theme.colors['$color-neutral-70']}; */
   display: flex;
   flex-direction: column;
   height: 100%;
